729b269e58ff3612247a9420d807653ebdd7b348
johan12361 <johan12361@gmail.com>
2024-07-08 09:47:50 -0500

    version 1.0.4

--- src/sistema/restAPI.mjs
+++ src/sistema/restAPI.mjs
@@ -1,20 +1,39 @@
 import 'dotenv/config'
 //TT MODULOS
-import { ACTUALIZAR } from './textos.mjs'
+import { ActuaInfo, ActuaMsj } from './textos.mjs'
 
 export function APIREST(PROV) {
-  // Ruta con parámetro de URL
-  PROV.server.get('/actualizar/:clave', async (req, res) => {
+  //TT ACTUALIZAR INFO
+  PROV.server.get('/actualizar/info/:clave', async (req, res) => {
     const clave = req.params.clave // Accede a los parámetros de la URL
     if (clave === process.env.REST_CLAVE_ACTUALIZAR) {
-      console.log(`DATO RECIBIDO: actualizar con clave = ${clave}`)
+      console.log(`DATO RECIBIDO: actualizar INFO con clave = ${clave}`)
       try {
-        await ACTUALIZAR()
-        console.log('la actualización se realizó con éxito.')
+        await ActuaInfo()
+        console.log('la actualización de INFO se realizó con éxito.')
         res.end('la actualización se realizó con éxito.')
       } catch (error) {
         console.error('Error durante la actualización:', error)
-        res.status(500).send('ocurrió un error durante la actualización.')
+        res.status(500).end('ocurrió un error durante la actualización.')
+      }
+    } else {
+      console.log(`la calve: ${clave} no es valida`)
+      res.end(`la calve: ${clave} no es valida`)
+    }
+  })
+
+  //TT ACTUALIZAR MENSAJES
+  PROV.server.get('/actualizar/msj/:clave', async (req, res) => {
+    const clave = req.params.clave // Accede a los parámetros de la URL
+    if (clave === process.env.REST_CLAVE_ACTUALIZAR) {
+      console.log(`DATO RECIBIDO: actualizar MENSAJES con clave = ${clave}`)
+      try {
+        await ActuaMsj()
+        console.log('la actualización de MENSAJES se realizó con éxito.')
+        res.end('la actualización se realizó con éxito.')
+      } catch (error) {
+        console.error('Error durante la actualización:', error)
+        res.status(500).end('ocurrió un error durante la actualización.')
       }
     } else {
       console.log(`la calve: ${clave} no es valida`)
--- src/sistema/textos.mjs
+++ src/sistema/textos.mjs
@@ -36,7 +36,53 @@ export async function ACTUALIZAR() {
   }
   console.table(MENSAJES)
   const inf = await ObtenerDatos(process.env.PAG_INFO)
+  if (inf !== null) {
+    INFO.RANGO_DIAS = parseInt(inf[0].RANGO_DIAS, 10)
+    INFO.NUMERO_CONTACTO = inf[0].NUMERO_CONTACTO
+    if (inf[0].NOTI_AGENDAR === 'true') {
+      INFO.NOTI_AGENDAR = true
+    } else {
+      INFO.NOTI_AGENDAR = false
+    }
+    if (inf[0].NOTI_CANCELAR === 'true') {
+      INFO.NOTI_CANCELAR = true
+    } else {
+      INFO.NOTI_CANCELAR = false
+    }
+    //SS NOTIFICACION DE RECORDATORIO
+    if (inf[0].NOTIFICAR === 'true') {
+      NOTIFICACION.NOTIFICAR = true
+    } else {
+      NOTIFICACION.NOTIFICAR = false
+    }
+    const _hora = inf[0].HORA_NOTI.split(':')
+    NOTIFICACION.HORA = parseInt(_hora[0], 10)
+    NOTIFICACION.MINUTO = parseInt(_hora[1], 10)
+  }
+  console.table(INFO)
+  console.table(NOTIFICACION)
+
+  //SS REINICIAR CRON
+  ReiniciarCron()
+}
+
+//TT ACTUALIZAR MENSAJES
+export async function ActuaMsj() {
+  const msj = await ObtenerDatos(process.env.PAG_MSJ)
   if (msj !== null) {
+    MENSAJES.SALUDO = msj[0].SALUDO
+    MENSAJES.ADIOS = msj[0].ADIOS
+    MENSAJES.RECORDATORIO = msj[0].RECORDATORIO
+    MENSAJES.SIN_CITAS_DISP = msj[0].CITAS_NO_DISPONIBLES
+    MENSAJES.AYUDA = msj[0].AYUDA
+  }
+  console.table(MENSAJES)
+}
+
+//TT ACTUALIZAR INFO
+export async function ActuaInfo() {
+  const inf = await ObtenerDatos(process.env.PAG_INFO)
+  if (inf !== null) {
     INFO.RANGO_DIAS = parseInt(inf[0].RANGO_DIAS, 10)
     INFO.NUMERO_CONTACTO = inf[0].NUMERO_CONTACTO
     if (inf[0].NOTI_AGENDAR === 'true') {
